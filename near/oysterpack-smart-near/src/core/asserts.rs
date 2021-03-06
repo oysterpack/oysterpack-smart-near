7648915dc59459d9562dadfd577058ac695f6fa2
oysterpack.inc <2zP6OaymGZhJTHFvQMzp6Ze>
2021-03-06 08:24:52 -0500

    working on AccountService - WIP

--- near/oysterpack-smart-near/src/core/asserts.rs
+++ near/oysterpack-smart-near/src/core/asserts.rs
@@ -11,20 +11,77 @@ const ERR_INSUFFICIENT_NEAR_DEPOSIT: ErrCode = ErrCode("INSUFFICIENT_NEAR_DEPOSI
 
 /// used to protect functions that transfer value against FCAK calls
 pub fn assert_yocto_near_attached() {
-    assert_eq!(
-        env::attached_deposit(),
-        1,
-        "{}",
-        ERR_YOCTONEAR_DEPOSIT_REQUIRED
-    )
+    if env::attached_deposit() != 1 {
+        env::panic(ERR_YOCTONEAR_DEPOSIT_REQUIRED.to_string().as_bytes())
+    }
 }
 
 /// used to protect functions that transfer value against FCAK calls
 pub fn assert_min_near_attached(min: YoctoNear) {
     assert!(
         env::attached_deposit() >= *min,
-        "{} attached NEAR amount is insufficient - min required amount is: {}",
+        "{} attached NEAR amount is insufficient - minimum required amount is: {} yoctoNEAR",
         ERR_INSUFFICIENT_NEAR_DEPOSIT,
         min
     )
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use oysterpack_smart_near_test::*;
+
+    #[test]
+    fn assert_yocto_near_attached_check_passes() {
+        let mut ctx = new_context("bob");
+        ctx.attached_deposit = 1;
+        testing_env!(ctx);
+
+        assert_yocto_near_attached();
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "[ERR] [YOCTONEAR_DEPOSIT_REQUIRED] exactly 1 yoctoNEAR must be attached"
+    )]
+    fn assert_yocto_near_attached_with_zero_deposit() {
+        let ctx = new_context("bob");
+        testing_env!(ctx);
+
+        assert_yocto_near_attached();
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "[ERR] [YOCTONEAR_DEPOSIT_REQUIRED] exactly 1 yoctoNEAR must be attached"
+    )]
+    fn assert_yocto_near_attached_with_2_deposit() {
+        let mut ctx = new_context("bob");
+        ctx.attached_deposit = 2;
+        testing_env!(ctx);
+
+        assert_yocto_near_attached();
+    }
+
+    #[test]
+    fn assert_min_near_attached_check_passes() {
+        let mut ctx = new_context("bob");
+        ctx.attached_deposit = 100;
+        testing_env!(ctx);
+
+        assert_min_near_attached(100.into());
+        assert_min_near_attached(50.into());
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "[ERR] [INSUFFICIENT_NEAR_DEPOSIT] attached NEAR amount is insufficient - minimum required amount is: 200 yoctoNEAR"
+    )]
+    fn assert_min_near_attached_insufficient_depoist() {
+        let mut ctx = new_context("bob");
+        ctx.attached_deposit = 199;
+        testing_env!(ctx);
+
+        assert_min_near_attached(200.into());
+    }
 }
--- near/oysterpack-smart-near/src/core/errors.rs
+++ near/oysterpack-smart-near/src/core/errors.rs
@@ -9,15 +9,28 @@ impl Display for ErrCode {
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Error(pub ErrCode, pub String);
+impl ErrCode {
+    /// constructs an [`Error`] using this [`ErrCode`] and the specified message
+    pub fn error<Msg: Display>(&self, msg: Msg) -> Error<Msg> {
+        Error(*self, msg)
+    }
+}
 
-impl Display for Error {
+#[derive(Debug, Clone, PartialEq)]
+pub struct Error<Msg>(pub ErrCode, pub Msg)
+where
+    Msg: Display;
+
+impl<Msg> Display for Error<Msg>
+where
+    Msg: Display,
+{
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{} {}", self.0, self.1)
     }
 }
 
+/// Error that can be defined as a constant, i.e., the error message is constant
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct ErrorConst(pub ErrCode, pub &'static str);
 
@@ -30,12 +43,25 @@ impl Display for ErrorConst {
 #[cfg(test)]
 mod test {
     use super::*;
-
-    const ERR_1: ErrCode = ErrCode("INVALID_ACCOUNT_ID");
+    use regex::Regex;
 
     #[test]
     fn error_display() {
-        println!("{}", Error(ERR_1, "BOOM".to_string()));
-        println!("{}", ErrorConst(ERR_1, "BOOM"));
+        const ERR: ErrCode = ErrCode("INVALID_ACCOUNT_ID");
+
+        println!("{}", Error(ERR, "BOOM".to_string()));
+        println!("{}", ErrorConst(ERR, "BOOM"));
+        println!("{}", ERR.error("BOOM"));
+    }
+
+    #[test]
+    fn err_display_format() {
+        let err_fmt_regex = Regex::new(r"\[ERR] \[\w+] \w+").unwrap();
+
+        const ERR: ErrCode = ErrCode("INVALID_ACCOUNT_ID");
+        let err = ERR.error("BOOM");
+        println!("{}", ERR.error("BOOM"));
+
+        assert!(err_fmt_regex.is_match(&err.to_string()));
     }
 }
