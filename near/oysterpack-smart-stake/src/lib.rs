abaa494129c1a524b929644c67d01a1cf214e19d
oysterpack.inc <2zP6OaymGZhJTHFvQMzp6Ze>
2021-03-08 15:08:04 -0500

    AccountService is still WIP

--- near/oysterpack-smart-stake/src/lib.rs
+++ near/oysterpack-smart-stake/src/lib.rs
@@ -21,8 +21,6 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[borsh_init(init)]
 pub struct Contract {
     #[borsh_skip]
-    key_value_store: KeyValueStoreService,
-    #[borsh_skip]
     account_service_module: AccountServiceModule,
 }
 
@@ -36,7 +34,6 @@ impl Contract {
             max: None,
         }));
         Self {
-            key_value_store: KeyValueStoreService,
             account_service_module: Default::default(),
         }
     }
@@ -58,66 +55,9 @@ impl Contract {
 }
 
 impl Contract {
-    /// get run each time the contract is loaded from storage and instantiated
+    /// gets run each time the contract is loaded from storage and instantiated
     fn init(&mut self) {
         eventbus::register(AccountStats::on_account_storage_event);
-        unsafe {
-            FOO = 1;
-        }
-    }
-}
-
-static mut FOO: u128 = 0;
-
-type Data = Object<u128, YoctoNear>;
-
-#[near_bindgen]
-impl KeyValueStore for Contract {
-    fn get(&self, key: U128) -> Option<YoctoNear> {
-        self.key_value_store.get(key)
-    }
-
-    fn set(&mut self, key: U128, value: YoctoNear) {
-        self.key_value_store.set(key, value)
-    }
-}
-
-trait KeyValueStore {
-    fn get(&self, key: U128) -> Option<YoctoNear>;
-
-    fn set(&mut self, key: U128, value: YoctoNear);
-}
-
-trait HasKeyValueStore {
-    fn get_key_value_store(&self) -> &dyn KeyValueStore;
-
-    fn get_mut_key_value_store(&mut self) -> &mut dyn KeyValueStore;
-}
-
-impl<T> KeyValueStore for T
-where
-    T: HasKeyValueStore,
-{
-    fn get(&self, key: U128) -> Option<YoctoNear> {
-        self.get_key_value_store().get(key)
-    }
-
-    fn set(&mut self, key: U128, value: YoctoNear) {
-        self.get_mut_key_value_store().set(key, value);
-    }
-}
-
-#[derive(Default)]
-struct KeyValueStoreService;
-
-impl KeyValueStore for KeyValueStoreService {
-    fn get(&self, key: U128) -> Option<YoctoNear> {
-        let foo = unsafe { FOO };
-        Data::load(&key.0).map(|object| YoctoNear::from(object.value() + foo))
-    }
-
-    fn set(&mut self, key: U128, value: YoctoNear) {
-        Data::new(key.0, value).save();
     }
 }
 
