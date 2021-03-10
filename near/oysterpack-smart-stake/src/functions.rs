3b562ba9c71b5c11a2d469ca7b275738d0497cb2
oysterpack.inc <2zP6OaymGZhJTHFvQMzp6Ze>
2021-03-10 11:35:43 -0500

    cleanup

--- near/oysterpack-smart-stake/src/functions.rs
+++ near/oysterpack-smart-stake/src/functions.rs
@@ -1,22 +1,43 @@
 use crate::*;
+use near_sdk::json_types::ValidAccountId;
 use near_sdk::near_bindgen;
-use oysterpack_smart_account_management::{AccountStorageEvent, StorageUsageBounds};
+use oysterpack_smart_account_management::{
+    AccountStorageEvent, StorageBalanceBounds, StorageManagement, StorageUsageBounds,
+};
 use oysterpack_smart_account_management::{AccountStorageUsage, StorageBalance};
+use oysterpack_smart_near::domain::YoctoNear;
 use oysterpack_smart_near::eventbus;
 
 #[near_bindgen]
-impl Contract {
-    pub fn simulate_account_storage_event(&self) {
-        eventbus::post(&AccountStorageEvent::Registered(
-            StorageBalance {
-                total: 100.into(),
-                available: 0.into(),
-            },
-            1000.into(),
-        ));
+impl StorageManagement for Contract {
+    #[payable]
+    fn storage_deposit(
+        &mut self,
+        account_id: Option<ValidAccountId>,
+        registration_only: Option<bool>,
+    ) -> StorageBalance {
+        self.context
+            .account_management
+            .storage_deposit(account_id, registration_only)
     }
 
-    pub fn storage_usage_bounds(&self) -> StorageUsageBounds {
-        self.context.account_management.storage_usage_bounds()
+    #[payable]
+    fn storage_withdraw(&mut self, amount: Option<YoctoNear>) -> StorageBalance {
+        self.context.account_management.storage_withdraw(amount)
+    }
+
+    #[payable]
+    fn storage_unregister(&mut self, force: Option<bool>) -> bool {
+        self.context.account_management.storage_unregister(force)
+    }
+
+    fn storage_balance_bounds(&self) -> StorageBalanceBounds {
+        self.context.account_management.storage_balance_bounds()
+    }
+
+    fn storage_balance_of(&self, account_id: ValidAccountId) -> Option<StorageBalance> {
+        self.context
+            .account_management
+            .storage_balance_of(account_id)
     }
 }
