8a4867d3272e5ee62db87bd98d5e4738e1cb4f7f
oysterpack.inc <2zP6OaymGZhJTHFvQMzp6Ze>
2021-03-22 15:00:33 -0400

    testing PermissionManagement interface on AccountManagement component - WIP

--- near/oysterpack-smart-account-management/src/components/account_management.rs
+++ near/oysterpack-smart-account-management/src/components/account_management.rs
@@ -15,7 +15,7 @@ use near_sdk::{
 use oysterpack_smart_near::{
     asserts::{assert_min_near_attached, assert_yocto_near_attached},
     domain::YoctoNear,
-    eventbus, ErrCode, ErrorConst,
+    eventbus, ErrCode, Error, ErrorConst,
 };
 use std::{fmt::Debug, ops::Deref};
 use teloc::*;
@@ -24,7 +24,7 @@ use crate::components::account_storage_usage::AccountStorageUsageComponent;
 use oysterpack_smart_near::asserts::{assert_account_not_predecessor, ERR_INVALID};
 use oysterpack_smart_near::component::Deploy;
 use oysterpack_smart_near::domain::{StorageUsage, ZERO_NEAR};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::marker::PhantomData;
 
 pub const ERR_INSUFFICIENT_STORAGE_BALANCE: ErrorConst = ErrorConst(
@@ -56,14 +56,24 @@ impl ContractPermissions {
     /// retains permission bits that are in the range 0-61
     /// - 62 -> operator
     /// - 63 -> admin
-    pub fn new(mut permissions: HashMap<u8, &'static str>) -> Self {
+    ///
+    /// ## Panics
+    /// - if permission names are not unique
+    /// - if permission bits >= 62 were specified
+    pub fn new(permissions: HashMap<u8, &'static str>) -> Self {
         let invalid_permissions: Vec<u8> = permissions
             .iter()
             .filter_map(|(k, _)| if *k >= 62_u8 { Some(*k) } else { None })
             .collect();
-        for invalid_perm in invalid_permissions {
-            permissions.remove(&invalid_perm);
-        }
+        assert!(
+            invalid_permissions.is_empty(),
+            "invalid permission bits were specified - the valid range is [0-61]"
+        );
+        let names: HashSet<_> = permissions.values().collect();
+        ERR_INVALID.assert(
+            || names.len() == permissions.len(),
+            || "permission names must be unique",
+        );
         Self(Some(permissions))
     }
 
@@ -77,7 +87,7 @@ impl ContractPermissions {
     }
 
     /// permissions will be returned as sorted
-    pub fn permission_labels(&self, permissions: Permissions) -> Vec<String> {
+    pub fn permission_names(&self, permissions: Permissions) -> Vec<String> {
         let mut labels = self.0.as_ref().map_or(vec![], |perms| {
             perms
                 .keys()
@@ -106,6 +116,43 @@ impl ContractPermissions {
         perms.sort();
         perms
     }
+
+    /// folds together the specified permissions specified by name into a [`Permissions']
+    pub fn fold_permissions(&self, permissions: Vec<String>) -> Result<Permissions, Error<String>> {
+        if permissions.is_empty() {
+            return Ok(0.into());
+        }
+
+        match self.0.as_ref() {
+            None => Err(ERR_INVALID.error("contract has no permissions".to_string())),
+            Some(perms) => {
+                let contract_perms: HashSet<String> =
+                    perms.values().map(|perm| perm.to_string()).collect();
+                let invalid_perms: Vec<String> = permissions
+                    .iter()
+                    .filter(|perm| {
+                        // let perm = perm.to_string();
+                        !contract_perms.contains(perm.as_str())
+                    })
+                    .map(|perm| perm.to_string())
+                    .collect();
+                if !invalid_perms.is_empty() {
+                    return Err(ERR_INVALID.error(format!(
+                        "contract does not support specified permissions: {:?}",
+                        invalid_perms
+                    )));
+                }
+                let perms = perms.iter().fold(HashMap::new(), |mut perms, (k, v)| {
+                    perms.insert(v.to_string(), *k);
+                    perms
+                });
+                let permissions: u64 = permissions.iter().fold(0_64, |permissions, perm| {
+                    permissions | 1 << *perms.get(perm).unwrap()
+                });
+                Ok(permissions.into())
+            }
+        }
+    }
 }
 
 /// [`ContractPermissions`] can be constructed by specifying (permission_bit, permission_label) mappings:
@@ -118,18 +165,26 @@ impl ContractPermissions {
 ///     (2, "PERM_2"),
 /// ].into();
 /// ```
-/// - any permission bits >= 62 will be filtered out (62 -> [`Permissions::OPERATOR`], 63 -> [`Permissions::ADMIN`])
+///
+/// ## Panics
+/// - if any permission bits >= 62
 impl From<Vec<(u8, &'static str)>> for ContractPermissions {
     fn from(values: Vec<(u8, &'static str)>) -> Self {
         let mut permissions = values
             .iter()
             .fold(HashMap::new(), |mut permissions, entry| {
-                if entry.0 < 62 {
-                    permissions.insert(entry.0, entry.1);
-                }
+                ERR_INVALID.assert(
+                    || entry.0 < 62,
+                    || "invalid permission bit - valid range is [0-61]",
+                );
+                permissions.insert(entry.0, entry.1);
                 permissions
             });
         permissions.shrink_to_fit();
+        ERR_INVALID.assert(
+            || permissions.len() == values.len(),
+            || "duplicate permission bits were specified",
+        );
         ContractPermissions::new(permissions)
     }
 }
@@ -531,7 +586,7 @@ where
             account.save();
             LOG_EVENT_PERMISSIONS_GRANT.log(format!(
                 "{:?}",
-                self.contract_permissions.permission_labels(permissions)
+                self.contract_permissions.permission_names(permissions)
             ));
         }
     }
@@ -548,7 +603,7 @@ where
             account.save();
             LOG_EVENT_PERMISSIONS_REVOKE.log(format!(
                 "{:?}",
-                self.contract_permissions.permission_labels(permissions)
+                self.contract_permissions.permission_names(permissions)
             ));
         }
     }
@@ -3257,6 +3312,27 @@ mod test_permission_management {
                             .ops_permissions_revoke(to_valid_account_id(bob), PERM_0.into());
                         let logs = test_utils::get_logs();
                         assert!(logs.is_empty());
+
+                        // Act - grant
+                        testing_env!(ctx.clone());
+                        account_manager.ops_permissions_grant(
+                            to_valid_account_id(bob),
+                            (PERM_0 | PERM_1).into(),
+                        );
+
+                        testing_env!(ctx.clone());
+                        account_manager.ops_permissions_revoke_all(to_valid_account_id(bob));
+                        let logs = test_utils::get_logs();
+                        println!("{:#?}", logs);
+                        assert_eq!(logs.len(), 2);
+                        assert_eq!(
+                            &logs[0],
+                            "[INFO] [ACCOUNT_STORAGE_CHANGED] StorageUsageChange(-8)"
+                        );
+                        assert_eq!(
+                            &logs[1],
+                            "[INFO] [PERMISSIONS_REVOKE] all permissions were revoked"
+                        );
                     });
                 }
             }
@@ -3323,6 +3399,12 @@ mod test_permission_management {
             });
         }
     }
+}
+
+#[cfg(test)]
+mod test_contract_permissions {
+    use super::*;
+    use near_sdk::test_utils;
 
     #[test]
     fn contract_permissions() {
@@ -3330,7 +3412,7 @@ mod test_permission_management {
         assert!(!contract_permissions.is_supported((1 << 15).into()));
         assert!(!contract_permissions.is_supported((1 << 0).into()));
         assert!(contract_permissions
-            .permission_labels((1 << 15).into())
+            .permission_names((1 << 15).into())
             .is_empty());
 
         const MINTER: Permission = 1 << 10;
@@ -3345,14 +3427,14 @@ mod test_permission_management {
         assert!(contract_permissions.is_supported(((1 << 10) | (1 << 20)).into()));
         assert!(!contract_permissions.is_supported(((1 << 10) | (1 << 15)).into()));
 
-        let labels = contract_permissions.permission_labels(((1 << 10) | (1 << 20)).into());
+        let labels = contract_permissions.permission_names(((1 << 10) | (1 << 20)).into());
         println!("{:?}", labels);
         assert_eq!(labels.len(), 2);
         assert!(labels.contains(&"minter".to_string()));
         assert!(labels.contains(&"burner".to_string()));
 
         let labels =
-            contract_permissions.permission_labels(((1 << 10) | (1 << 20) | (1 << 15)).into());
+            contract_permissions.permission_names(((1 << 10) | (1 << 20) | (1 << 15)).into());
         assert_eq!(labels.len(), 2);
         assert!(labels.contains(&"minter".to_string()));
         assert!(labels.contains(&"burner".to_string()));
@@ -3361,5 +3443,44 @@ mod test_permission_management {
         assert_eq!(perms.len(), 2);
         assert!(perms.contains(&MINTER.into()));
         assert!(perms.contains(&BURNER.into()));
+    }
+
+    #[test]
+    #[should_panic(expected = "[ERR] [INVALID] invalid permission bit - valid range is [0-61]")]
+    fn create_with_invalid_bits() {
+        test_utils::test_env::setup();
+        let _contract_permissions: ContractPermissions = vec![(62_u8, "invalid")].into();
+    }
+
+    #[test]
+    #[should_panic(expected = "[ERR] [INVALID] duplicate permission bits were specified")]
+    fn create_with_duplicate_bits() {
+        test_utils::test_env::setup();
+        let _contract_permissions: ContractPermissions = vec![(1, "1"), (1, "1")].into();
+    }
+
+    #[test]
+    #[should_panic(expected = "[ERR] [INVALID] permission names must be unique")]
+    fn create_with_duplicate_perm_names() {
+        test_utils::test_env::setup();
+        let _contract_permissions: ContractPermissions = vec![(1, "1"), (2, "1")].into();
+    }
+
+    #[test]
+    fn fold_permissions() {
+        test_utils::test_env::setup();
+        let contract_permissions: ContractPermissions = vec![(1, "1"), (2, "2"), (3, "3")].into();
+        let perm_123 = contract_permissions
+            .fold_permissions(vec![
+                "1".to_string(),
+                "2".to_string(),
+                "3".to_string(),
+                "3".to_string(),
+            ])
+            .unwrap();
+        assert_eq!(
+            contract_permissions.unfold_permissions(perm_123),
+            vec![(1 << 1).into(), (1 << 2).into(), (1 << 3).into()]
+        );
     }
 }
