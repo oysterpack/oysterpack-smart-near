742442682d7df9a126e290fb7f399f232a6b6d4e
oysterpack.inc <2zP6OaymGZhJTHFvQMzp6Ze>
2021-03-30 05:11:40 -0400

    working on staking pool component

--- near/oysterpack-smart-account-management/src/domain.rs
+++ near/oysterpack-smart-account-management/src/domain.rs
@@ -1,7 +1,7 @@
 pub use account_data::*;
 pub use account_metrics::*;
 pub use account_near_data::*;
-pub use account_storage_usage_event::*;
+pub use account_storage_event::*;
 pub use oysterpack_smart_near::domain::AccountIdHash;
 pub use permissions::*;
 pub use storage_balance::*;
@@ -11,7 +11,7 @@ pub use storage_usage_bounds::*;
 mod account_data;
 mod account_metrics;
 mod account_near_data;
-mod account_storage_usage_event;
+mod account_storage_event;
 mod permissions;
 mod storage_balance;
 mod storage_balance_bounds;
